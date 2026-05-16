b883595b6605071a073e961b42be34a351e60b86
vrenken <github@vrenken.eu>
2026-05-16 14:40:58 +0200

    Added ThrowingErrorListener class to handle syntax errors in tests.

---
+++ dotnet/source/EtAlii.Yagl.Tests/ThrowingErrorListener.cs
@@ -0,0 +1,11 @@
+﻿using Antlr4.Runtime;
+
+namespace EtAlii.Yagl.Tests;
+
+public class ThrowingErrorListener : BaseErrorListener
+{
+    public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+    {
+        throw new Exception($"Syntax error at line {line}:{charPositionInLine} - {msg}");
+    }
+}
